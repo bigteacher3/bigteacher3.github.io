ac2c1fcdda44ee3eed8f02a4af97678ea20848d0
bigteacher3 <admin@loz3edu.pp.ua>
2025-05-21 21:23:10 +0300

    Update List2.py

--- prog/List2.py
+++ prog/List2.py
@@ -1,7 +1,6 @@
 friday_schedule = ["Математика", "Англійська мова", "Історія", "Фізика", "Інформатика"]
-
-print("Розклад (список):", friday_schedule)
-
+k = len(friday_schedule)
 print("Розклад (по одному уроку):")
-for lesson in friday_schedule:
-    print(lesson)
+for lesson, n in zip(friday_schedule, range(k)):
+    print(f"{n+1}) {lesson}")
+print(f"Всього {k} уроків")
